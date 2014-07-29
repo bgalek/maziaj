4dd365b3c7330b3cafef62d456dbc19c4c6094c5
Bartosz Gałek <bartosz.galek@allegro.pl>
2014-07-29 12:04:50 +0200

    preparing files to start working

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var env = process.env.NODE_ENV || 'production';
 
+if (env === 'development') {
+    console.log("DEVELOPMENT MODE");
+}
+
+//express
+var express = require('express'),
+    path = require('path'),
+    favicon = require('static-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    http = require('http');
+
+//routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+//app
+var app = module.exports = express();
 
-// view engine setup
+//configuration
+var oneDay = 86400000;
+
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -26,7 +39,7 @@ app.use('/', routes);
 app.use('/users', users);
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -37,7 +50,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -48,7 +61,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
@@ -56,5 +69,6 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
-module.exports = app;
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
