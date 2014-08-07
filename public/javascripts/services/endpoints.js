f46db462dfa2f7e4740e1264575acb5bc2344e42
Bartosz Gałek <bartosz.galek@allegro.pl>
2014-08-07 22:03:04 +0200

    reverted bad api url

--- public/javascripts/services/endpoints.js
+++ public/javascripts/services/endpoints.js
@@ -1,6 +1,6 @@
 "use strict";
 maziajApp.factory('endpoints', function () {
-    var apiEndpoint = 'http://localhost:8080/';
+    var apiEndpoint = 'http://api-maziaj.herokuapp.com/';
     return {
         chainsList: function (page) {
             return apiEndpoint + 'chain' + '?page=' + page;
